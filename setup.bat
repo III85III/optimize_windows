6c47bcc5d8e4b2698db8fd4d9b2b80035ec116b8
III85III <III85III@users.noreply.github.com>
2026-08-30 02:57:44 +0330

    Add setup.bat launcher

---
+++ setup.bat
@@ -0,0 +1,10 @@
+@echo off
+cd /d "%~dp0"
+title III85III - Setup & Run
+color 0E
+
+echo ==========================================
+echo    Launching VelocityKit...
+echo ==========================================
+call run.bat
+pause
