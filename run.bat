f37e611f0a539376e8df560d7f33d7d9e8623270
III85III <III85III@users.noreply.github.com>
2026-08-29 19:08:43 +0330

    Fix menu navigation in run.bat

--- run.bat
+++ run.bat
@@ -13,11 +13,13 @@ echo 6. Exit
 echo ==========================================
 set /p choice="Enter your choice (1-6): "
 
-if "%choice%"=="1" scripts\clean_cache_and_ram.bat
-if "%choice%"=="2" scripts\clear_cache_0.bat
-if "%choice%"=="3" scripts\disable_bloatware.bat
-if "%choice%"=="4" scripts\extreme_debloat.bat
-if "%choice%"=="5" scripts\run_god_mode.bat
+if "%choice%"=="1" call scripts\clean_cache_and_ram.bat & pause & goto menu
+if "%choice%"=="2" call scripts\clear_cache_0.bat & pause & goto menu
+if "%choice%"=="3" call scripts\disable_bloatware.bat & pause & goto menu
+if "%choice%"=="4" call scripts\extreme_debloat.bat & pause & goto menu
+if "%choice%"=="5" call scripts\run_god_mode.bat & pause & goto menu
 if "%choice%"=="6" exit
 
+echo Invalid choice, try again.
+pause
 goto menu
